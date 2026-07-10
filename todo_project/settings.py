059627d42ae89829e2d2021075273903f4ac450b
Malual Martin Biar <malual.martinbiar@students.mak.ac.ug>
2026-07-10 10:49:44 +0200

    Updated settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -38,11 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Third party apps
     'rest_framework',
     'drf_yasg',
 
-    # Local apps
     'todo_api',
 ]
 
